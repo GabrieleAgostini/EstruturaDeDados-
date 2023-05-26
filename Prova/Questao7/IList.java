e809cf271b727e07479cd1e0fa1d2861debb8b2d
Gabriele Agostini <100322510+GabrieleAgostini@users.noreply.github.com>
2023-05-26 00:07:16 -0300

    Prova Questão 7

---
+++ Prova/Questao7/IList.java
@@ -0,0 +1,14 @@
+package Questao7;
+
+public interface IList {
+    
+    public void addBegin(Object info);
+    public void addEnd(Object info);
+    public boolean isEmpty();
+    public boolean remove(Object info);
+    public int size();
+    public void print();
+    public Object getFirst();
+    public Object getLast();
+    
+}
---
+++ Prova/Questao7/Main.java
@@ -0,0 +1,17 @@
+package Questao7;
+
+public class Main {
+	public static void main(String[] args) {
+		LSE lista = new LSE();
+
+		lista.addEnd(1);
+		lista.addEnd(2);
+		lista.addEnd(3);
+		lista.addEnd(4);
+		
+		System.out.println("This is the list: ");
+		lista.imprime_rec();
+
+	}
+
+}
